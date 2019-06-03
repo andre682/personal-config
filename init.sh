9bf3e391d62b7ac1ff35edf906fcb745fcf98932
André Augusto <andre682@gmail.com>
2019-06-03 16:30:18 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#sh -c "$(curl -fsSL https://raw.github.com/andre682/personal-config/master/init.sh)"
+
 # Default settings
 DIRECTORY=${DIRECTORY:-~/personal-config}
 MAINREPO=${MAINREPO:-andre682/personal-config}
@@ -8,15 +10,16 @@ MAINBRANCH=${MAINBRANCH:-master}
 
 echo "APT-GET UPDATE..."
 apt-get update -qq
-echo "DEPENDENCIES..."
-apt-get install zsh -y -qq
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Install node"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
 
 git clone --depth=1 --branch "$MAINBRANCH" "$MAINREMOTE" "$DIRECTORY"
 
-cp $DIRECTORY/ZSH/.zshrc ~/.zshrc
 
-rm -rf $DIRECTORY
+
+# rm -rf $DIRECTORY
+
+echo "DEPENDENCIES..."
+apt-get install zsh -y -qq
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
