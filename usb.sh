1ce3868dc516367fc0476f3e58382b9e708f5687
André Augusto <andre682@gmail.com>
2019-11-21 00:09:43 +0000

    Create usb.sh

---
+++ usb.sh
@@ -0,0 +1,9 @@
+# Inclui repositório universe
+
+sudo add-apt-repository universe
+
+
+# Allow videos on Firefox
+
+sudo apt install libavcodec-extra
+sudo apt install libdvdnav4 libdvdread4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
