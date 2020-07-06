bc78e6a7c74aa1ff42bc770c7dc8aa5f71a80486
André Augusto <andre682@gmail.com>
2020-07-06 11:22:08 -0300

    Create packages-development.sh

---
+++ packages-development.sh
@@ -0,0 +1,16 @@
+sudo apt-get install -y nodejs
+apt-get install -y build-essential
+
+  sudo apt install python2
+  sudo apt install playerctl
+
+sudo apt install postgresql postgresql-contrib
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common\n
+
+sudo apt install libpq-dev g++ make
+sudo apt install htop
+sudo apt install pgadmin4
+sudo apt install pavucontrol
+sudo apt install graphviz
+sudo apt-get install ubuntu-restricted-extras
